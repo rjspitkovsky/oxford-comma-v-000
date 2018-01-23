0c57c48195d9d8ae840f0f7c919e421609145455
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 21:09:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    return array[0..1].join(", ") + ", and " + array[2]
+  else array.length > 3
+    return array[0..-2].join(", ") + ", and " + array[-1]
+  end  
 end
