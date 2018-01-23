00e54fe8a47e34da84edd54279377b48d111e134
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 21:08:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,7 @@ def oxford_comma(array)
   end
 
   if array.length > 3
-    return array[0..-1].join(", ") + ", and" + array[-1]
-  end 
+    return array[0..-2].join(", ") + ", and" + array[-1]
+  end
 
 end
